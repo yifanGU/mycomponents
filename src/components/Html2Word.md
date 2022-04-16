090c76e13af03300556bc12143ec565d006754e2
yifanGU <guyifan1@126.com>
2022-04-16 20:13:59 +0800

    4.16 update html-2-word preload function

--- src/components/Html2Word.md
+++ src/components/Html2Word.md
@@ -25,3 +25,27 @@
         </template>
     </html-2-word>
 ```
+- （可选）html-2-word中的内容在父组件完成加载的时候，就已经确定好了。如果父组件在html2-2-word组件渲染完成后，点击按钮前这个阶段发生了更新，那么就会造成导出的word内容过时的情况出现。所以，html-2-word允许传入一个回调函数，用来在导出word文档前更新v-slot：content中的内容。
+```
+父组件html模板中：
+<html-2-word msg="Welcome to Your Vue.js App" :preLoad='getPicture'>
+<template  v-slot:content>
+    文字测试：
+    我是内容
+    图片测试：
+    <img id='pictureLoader' src=''>
+</template>
+
+<template v-slot:button proLoad:getPicture >
+    <button  class='btn btn-primary'>
+        {{ "hhh下载" }}
+    </button>
+</template>
+</html-2-word>
+父组件的script标签中：
+getPicture(){
+      let img = document.getElementById("pictureLoader");
+      img.src = "data:image/x-icon;base64,AAABAAEAICAAAAEAIAC......";
+    }
+  
+```
--- src/components/Html2Word.vue
+++ src/components/Html2Word.vue
@@ -7,7 +7,7 @@
                 </div>
             </slot>
         </div>
-        <div @click="exportHTML">
+        <div @click="exportHTML(preLoad)">
             <slot name="button">
                 <button  class='btn btn-primary'>
                     {{ "下载" }}
@@ -21,29 +21,60 @@
 <script>
 export default {
     name: 'Html2Word',
+    props: {
+        /* preLoad是预加载回调函数，用来更新source-html的内容
+         */
+        preLoad: {
+            type: Function,
+            default: ()=>{
+                console.log("没有preload函数,这比较适合静态的页面快照");
+            }
+        }
+    },
     methods:{
         /*
         * 将html转换为word
+        * fun函数的功能是获得source-html的内容。
+        * 如果有preLoad函数，那么在执行fun函数前，会执行preLoad函数。
+        * @param preLoad: function
+        * @return void
         */
-        exportHTML(){
-            let header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' "+
+        exportHTML(preLoad){
+            const fun = ()=>{
+                let header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' "+
                 "xmlns:w='urn:schemas-microsoft-com:office:word' "+
                 "xmlns='http://www.w3.org/TR/REC-html40'>"+
                 "<head><meta charset='utf-8'><title>Export HTML to Word Document with JavaScript</title></head><body>";
-            let footer = "</body></html>";
-            let sourceHTML = header+document.getElementById("source-html").innerHTML+footer;
+                let footer = "</body></html>";
+                let sourceHTML = header+document.getElementById("source-html").innerHTML+footer;
+                
+                let source = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(sourceHTML);
+                let fileDownload = document.createElement("a");
+                document.body.appendChild(fileDownload);
+                fileDownload.href = source;
+                fileDownload.download = 'document.doc';
+                fileDownload.click();
+                document.body.removeChild(fileDownload);
+            }
+
+            if(preLoad){
+                const workflow = new Promise((resolve, reject) => {
+                    try{
+                        preLoad();
+                        resolve();
+                    }catch(error){
+                        reject(error);
+                    }
+                })
+                workflow.then(() => { fun() })
+            }else{
+                fun()
+            }
             
-            let source = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(sourceHTML);
-            let fileDownload = document.createElement("a");
-            document.body.appendChild(fileDownload);
-            fileDownload.href = source;
-            fileDownload.download = 'document.doc';
-            fileDownload.click();
-            document.body.removeChild(fileDownload);
+        }
     },
     mounted(){
-        this.exportHTML();
-    }
+        // this.exportHTML();
     }
 }
 </script>
